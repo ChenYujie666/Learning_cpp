00a9d722bb032d3a69cdb3eb9fee7ea0439096b5
Sanwen <MarsCYJ@outlook.com>
2024-11-25 22:40:08 +0800

    【1】rebase files

--- cpp_primer/part1/part0/chap1/1_04.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-int main(){
-int a = 1, b = 2;
-
-std::cin >> a >> b;
-std::cout << "a+b=" << a+b << std::endl;
-std::cout << "a*b=" << a*b << std::endl;
-}
--- cpp_primer/part1/part0/chap1/1_05.cpp
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-int main(){
-int a = 1, b = 2;
-std::cout << "a+b=" << a+b << std::endl << "a*b=" << a*b << std::endl;
-}
