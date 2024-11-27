84cc1679036f210bba73511294cdfa6fa62fca26
Sanwen <MarsCYJ@outlook.com>
2024-11-27 23:10:05 +0800

    调整获取状态和操作的顺序

--- cpp_primer/part1/chap8/08_01_02.cpp
+++ cpp_primer/part1/chap8/08_01_02.cpp
@@ -8,22 +8,29 @@ istream& func(istream& is){
 	auto is_state = is.rdstate();
 	while(!is_state){
 		is >> i;
-		cout << "this is the " << ++index << "th run." << endl;
-		cout << i << "\t" << endl;
-		cout << endl;
+
 		is_state = is.rdstate();
-		// 为什么这边会多运行一次?? confusing 啊
-		// why this run again after inputing "eof"
-		
-		// is.clear(1);		
-		/*
-		if (is.fail()){
-			cerr << "Data Error! Please try again" << endl;
-			is.clear();
-			// is.ignore(100,"\n");
-			continue;
+
+		if(!is_state){
+			cout << "this is the " << ++index << "th run." << endl;
+			cout << i << "\t" << endl;
+			cout << endl;
+			// 为什么这边会多运行一次?? confusing 啊
+			// why this run again after inputing "eof"
+			// fk 要先进行操作，在进行状态判断
+			// first is>>i; next is_state = is.rdstate();
+			 
+			// is.clear(1);		
+			/*
+			if (is.fail()){
+				cerr << "Data Error! Please try again" << endl;
+				is.clear();
+				// is.ignore(100,"\n");
+				continue;
+			}
+			*/
 		}
-		*/
+
 	}
 	is.clear();
 	// cout << i << endl;
